b839eaec7a61a0b8a3587578c03061fa24008ee6
dan <875126243@qq.com>
2024-02-22 16:54:44 +0800

    fix update code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { Routes, Route, NavLink, Navigate } from 'react-router-dom'
+import React from 'react'
+import { NavLink, useRoutes } from 'react-router-dom'
+import routes from './router'
 
-import './App.css';
-import Home from './views/Home'
-import About from './views/About'
-import NoPage from './views/NoPage'
-import Detail from './views/Detail'
-import Admin from './views/Admin';
+const App = () => {
+  const routeElement = useRoutes(routes)
 
-
-function App() {
   return (
-    <div className="App">
-          <div>
-          <NavLink end to="/" state={{ id: 1000 }}>首页</NavLink>
-          <span> --- </span>
-          <NavLink to="/about">关于</NavLink>
-          <span> --- </span>
-          <NavLink to="/admin">后台</NavLink>
+    <div>
+      <div>
+        <NavLink to="/home">首页</NavLink>
+        <span> --- </span>
+        <NavLink to="/about">关于</NavLink>
+        <span> --- </span>
+        <NavLink to="/admin">后台</NavLink>
       </div>
-       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/admin' element={<Admin />} />
-        {/* 动态路由参数定义 */}
-        <Route path="/detail/:id" element={<Detail />} />
-         {/* 重定向 */}
-         <Route path="/" element={<Navigate to="/home" replace={false} />} />
-        {/* 404页面处理 */}
-        <Route path="*" element={<NoPage/>} />
-
-      </Routes>
+      <hr />
+      {routeElement}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,42 @@
-import { Navigate } from "react-router-dom";
+import { Navigate } from 'react-router-dom'
+
+import Home from '../views/Home'
+import About from '../views/About'
+import Detail from '../views/Detail'
+import Admin from '../views/Admin'
+import Welcome from '../views/Welcome'
+import User from '../views/User'
+
+const router = [
+  { path: '/', element: <Navigate to="/home" replace /> },
+  {
+    path: '/home',
+    element: <Home />
+  },
+  {
+    path: '/about',
+    element: <About />
+  },
+  {
+    path: '/detail/:id',
+    element: <Detail />
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
+    children: [
+      { index: true, element: <Navigate to="/admin/welcome" replace /> },
+      {
+        path: 'welcome',
+        element: <Welcome />
+      },
+      {
+        path: 'user',
+        element: <User />
+      }
+    ]
+  }
+]
 
 
-import Home from "../views/Home";
+export default router;
