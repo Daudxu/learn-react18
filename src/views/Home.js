f9bb3f604898aca0fc919efdec5c81cf1918cf5c
dan <875126243@qq.com>
2024-02-22 13:44:46 +0800

    fix update code

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,12 @@
 import '../App.css'
 import logo from '../logo.svg';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams, useParams, useLocation } from 'react-router-dom'
 
 function Home() {
+    const location = useLocation()
   const navigate = useNavigate()
+  const params = useParams()
+  const [search, setSearch] = useSearchParams()
 
   return (
     <div className="App">
@@ -28,6 +31,18 @@ function Home() {
         }}>
             回退
         </button>
+        <h3>页面详情</h3>
+      <div>动态路由参数id：{params.id}</div>
+      <div>state中的数据：{location.state?.name}</div>
+      <div>search字符串中的age：{search.get('age')}</div>
+        <button
+        onClick={() => {
+          // 修改之后state中的数据会丢失
+          setSearch('?age=100')
+        }}
+      >
+        修改search字符串
+      </button>
       </div>
     </div>
   );
